a4bbc28a757164fedc6e139aeebdfa329b68323c
Marlon Müller <marmueller@united-internet.de>
2022-01-27 14:06:08 +0100

    Add test for missing keys

--- cmd/root_test.go
+++ cmd/root_test.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"bytes"
+	"errors"
 	"os"
 	"template-renderer/test"
 	"testing"
@@ -87,6 +88,19 @@ func TestMultipleData(t *testing.T) {
 	file, err := os.ReadFile(testDir(t) + "/test.txt")
 	test.AssertEqual(t, nil, err)
 	test.AssertEqual(t, "A=1\nC=3", string(file))
+}
+
+func TestMissingData(t *testing.T) {
+	defer os.RemoveAll(TempPath)
+
+	var buffer bytes.Buffer
+	rootCmd.SetOut(&buffer)
+	rootCmd.SetArgs([]string{"-i", "../test/data/templates", "-o", testDir(t), "-d", yamlData1, "-t", ".template3"})
+
+	err := rootCmd.Execute()
+
+	test.AssertNotEqual(t, nil, err)
+	test.AssertNotEqual(t, errors.New("template: test.txt.template3:2:5: executing \"test.txt.template3\" at <.c>: map has no entry for key \"c\""), err)
 }
 
 func testDir(t *testing.T) string {
